4fa67a05c42f5408a2dfa00f974eec3f16c40628
shadowDragon <shadowdragon@localhost.localdomain>
2018-10-20 18:41:42 +0300

    Just creating bullets..nothing else,the rest later

---
+++ Zombie Escape Pravets/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+	public GameObject bullet;
+	///public Camera cam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			///Camera cam = Camera.main;
+			Instantiate(bullet, transform.position, transform.rotation);
+			}
+	}
+}
